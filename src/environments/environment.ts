be521641d0f544c2abafd5390692c9ba3a944d81
Vishnu Mali <vishnumali3911@gmail.com>
2023-02-13 14:00:31 +0530

    deployed worker

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   API_URL: 'https://mysql-tunnel-example-dev.vishnubhai.workers.dev/',
   enableAutoSuggest: false
 };
